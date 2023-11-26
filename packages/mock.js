a45359929cfcd18b83443cffde2dafdd401106fe
David Pitman <david.pitman@cloudouble.com>
2023-11-26 23:01:20 +1000

    building

--- packages/mock.js
+++ packages/mock.js
@@ -15,7 +15,42 @@ export default {
                 auth: {},
                 profiles: {},
                 users: {},
-                articles: {},
+                articles: {
+                    "how-to-train-your-dragon": {
+                        "slug": "how-to-train-your-dragon",
+                        "title": "How to train your dragon",
+                        "description": "Ever wonder how?",
+                        "body": "It takes a Jacobian",
+                        "tagList": ["dragons", "training"],
+                        "createdAt": "2016-02-18T03:22:56.637Z",
+                        "updatedAt": "2016-02-18T03:48:35.824Z",
+                        "favorited": false,
+                        "favoritesCount": 0,
+                        "author": {
+                            "username": "jake",
+                            "bio": "I work at statefarm",
+                            "image": "https://i.stack.imgur.com/xHWG8.jpg",
+                            "following": false
+                        }
+                    },
+                    "how-to-train-your-dragon-2": {
+                        "slug": "how-to-train-your-dragon-2",
+                        "title": "How to train your dragon 2",
+                        "description": "So toothless",
+                        "body": "It a dragon",
+                        "tagList": ["dragons", "training"],
+                        "createdAt": "2016-02-18T03:22:56.637Z",
+                        "updatedAt": "2016-02-18T03:48:35.824Z",
+                        "favorited": false,
+                        "favoritesCount": 0,
+                        "author": {
+                            "username": "jake",
+                            "bio": "I work at statefarm",
+                            "image": "https://i.stack.imgur.com/xHWG8.jpg",
+                            "following": false
+                        }
+                    }
+                },
                 comments: {}
             },
             "/api/users": function (context, payload) {
