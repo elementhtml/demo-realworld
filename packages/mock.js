1a238e0711a89b85dfcb8443f79d5f7eb25f510f
David Pitman <david.pitman@cloudouble.com>
2023-11-28 14:33:01 +1000

    building

--- packages/mock.js
+++ packages/mock.js
@@ -11,7 +11,7 @@ export default {
     },
     variables: {
         mock: {
-            data: {
+            baseData: {
                 auth: {},
                 profiles: {},
                 users: {},
@@ -24,13 +24,13 @@ export default {
                         "tagList": ["dragons", "training"],
                         "createdAt": "2016-02-18T03:22:56.637Z",
                         "updatedAt": "2016-02-18T03:48:35.824Z",
-                        "favorited": false,
+                        "favorited": [],
                         "favoritesCount": 0,
                         "author": {
                             "username": "jake",
                             "bio": "I work at statefarm",
                             "image": "https://i.stack.imgur.com/xHWG8.jpg",
-                            "following": false
+                            "following": []
                         }
                     },
                     "how-to-train-your-dragon-2": {
@@ -41,28 +41,42 @@ export default {
                         "tagList": ["dragons", "training"],
                         "createdAt": "2016-02-18T03:22:56.637Z",
                         "updatedAt": "2016-02-18T03:48:35.824Z",
-                        "favorited": false,
+                        "favorited": [],
                         "favoritesCount": 0,
                         "author": {
                             "username": "jake",
                             "bio": "I work at statefarm",
                             "image": "https://i.stack.imgur.com/xHWG8.jpg",
-                            "following": false
+                            "following": []
                         }
                     }
                 },
                 comments: {}
+            },
+            data: function (scope, path, value) {
+                if (!scope) return
+                let storageData = JSON.parse(window.sessionStorage.getItem('data') ?? 'null') ?? this.baseData
+                if (value === undefined) return path ? (storageData[scope] ?? {})[path] : storageData[scope] ?? {}
+                storageData[scope] ||= {}
+                if (value === null) {
+                    delete storageData[scope][path]
+                } else { storageData[scope][path] = value }
+                window.sessionStorage.setItem('data', JSON.stringify(storageData))
+            },
+            getRequestingUser: function (context) {
+                const token = (context?.headers?.Authorization ?? '').slice(6), requestingUsername = token ? this.data('auth', token) : undefined
+                return requestingUsername ? this.data('users', requestingUsername) : undefined
             },
             "/api/users": function (context, payload) {
                 if (typeof payload === 'string') try { payload = JSON.parse(payload) } catch (e) { return { errors: { payload: ['is not valid JSON'] } } }
                 switch (context.method) {
                     case 'POST':
                         if (payload?.user?.username) {
-                            this.data.users[payload.user.username] = payload.user
+                            this.data('users', payload.user.username, payload.user)
                         } else { return { errors: { username: ['is required'] } } }
-                        const user = this.data.users[payload.user.username]
-                        user.token = crypto.randomUUID()
-                        this.data.auth[user.token] = payload.user.username
+                        const user = this.data('users', payload.user.username), token = crypto.randomUUID()
+                        this.data('users', payload.user.username, { ...user, token })
+                        this.data('auth', token, payload.user.username)
                         return { user }
                 }
             },
@@ -71,23 +85,25 @@ export default {
                 switch (context.method) {
                     case 'POST':
                         if (!payload.user.email || !payload.user.password) return { errors: { user: ['email and password required'] } }
-                        const user = Object.values(this.data.users).filter(u => u.email == payload.user.email && u.password == payload.user.password)[0]
+                        const user = Object.values(this.data('users')).filter(u => u.email == payload.user.email && u.password == payload.user.password)[0]
                         if (user) {
-                            user.token = crypto.randomUUID()
-                            this.data.auth[user.token] = user.username
+                            const token = crypto.randomUUID()
+                            this.data('users', user.username, { ...user, token })
+                            this.data('auth', token, user.username)
                         } else { return { errors: { user: [`with matching email and password not found`] } } }
                         return { user }
                 }
             },
             "/api/user": function (context, payload) {
                 if (typeof payload === 'string') try { payload = JSON.parse(payload) } catch (e) { return { errors: { payload: ['is not valid JSON'] } } }
-                const token = (context?.headers?.Authorization ?? '').slice(6), username = token ? this.data.auth[token] : undefined,
-                    user = username ? this.data.users[username] : undefined
+                const user = this.getRequestingUser(context)
+                if (!user) return { errors: { user: ['not found'] } }
                 switch (context.method) {
                     case 'GET':
                         return user
                     case 'PUT':
                         if (user && payload) Object.assign(user, payload)
+                        this.data('users', user.username, user)
                         return user
                 }
             },
@@ -96,20 +112,20 @@ export default {
                 let profile = {}
                 switch (context.method) {
                     case 'GET':
-                        const username = (context?.url?.pathname ?? '').replace('//', '/').split('/')[3], user = username ? this.data.users[username] : undefined
+                        const username = (context?.url?.pathname ?? '').replace('//', '/').split('/')[3], user = username ? this.data('users', username) : undefined
                         if (user) for (const p of ['username', 'bio', 'image', 'following']) profile[p] = user[p]
                         break
                     case 'POST':
                     case 'DELETE':
-                        const token = (context?.headers?.Authorization ?? '').slice(6), requestingUsername = token ? this.data.auth[token] : undefined,
-                            requestingUser = requestingUsername ? this.data.users[requestingUsername] : undefined
-                        if (!requestingUser) return
+                        const requestingUser = this.getRequestingUser(context)
+                        if (!requestingUser) return { errors: { user: ['not authorized'] } }
                         const [, , , usernameTo, follow] = (context?.url?.pathname ?? '').replace('//', '/').split('/'),
-                            userTo = usernameTo ? this.data.users[usernameTo] : undefined
+                            userTo = usernameTo ? this.data('users', usernameTo) : undefined
                         if (userTo && follow) {
                             requestingUser.following ||= []
                             if (context.method === 'POST') requestingUser.following.push(usernameTo)
                             if (context.method === 'DELETE') requestingUser.following = requestingUser.following.filter(u => u != usernameTo)
+                            this.data('users', requestingUsername, requestingUser)
                         }
                         for (const p of ['username', 'bio', 'image', 'following']) profile[p] = userTo[p]
                 }
@@ -119,18 +135,16 @@ export default {
                 if (typeof payload === 'string') try { payload = JSON.parse(payload) } catch (e) { return { errors: { payload: ['is not valid JSON'] } } }
                 switch (context.method) {
                     case 'GET':
-                        console.log('mock.js: line 122', context, payload)
-                        let articles = Object.values(this.data.articles), searchParams = context?.url?.searchParams,
+                        let articles = Object.values(this.data('articles')), searchParams = context?.url?.searchParams,
                             filters = searchParams ? Object.fromEntries(searchParams.entries()) : undefined
                         const [, , , feedOrSlug, comments] = (context?.url?.pathname ?? '').replace('//', '/').split('/')
                         if (feedOrSlug === 'feed') {
-                            const token = (context?.headers?.Authorization ?? '').slice(6), requestingUsername = token ? this.data.auth[token] : undefined,
-                                requestingUser = requestingUsername ? this.data.users[requestingUsername] : undefined
+                            const requestingUser = this.getRequestingUser(context)
                             if (requestingUser) articles = articles.filter(a => (requestingUser.follows ?? []).includes(a.author?.username))
                         } else if (feedOrSlug && comments) {
-                            return this.data.comments[feedOrSlug] ?? []
+                            return this.data('comments', feedOrSlug) ?? []
                         } else if (feedOrSlug) {
-                            return this.data.articles[feedOrSlug]
+                            return this.data('articles', feedOrSlug)
                         }
                         if (articles.length && filters && Object.keys(filters).length) {
                             articles = articles.filter(a => {
@@ -147,10 +161,9 @@ export default {
                     case 'POST':
                     case 'PUT':
                     case 'DELETE':
-                        const token = (context?.headers?.Authorization ?? '').slice(6), requestingUsername = token ? this.data.auth[token] : undefined,
-                            requestingUser = requestingUsername ? this.data.users[requestingUsername] : undefined
-                        if (!requestingUser) return
-                        const [, , , articleSlug, commentsOrFavorite] = (context?.url?.pathname ?? '').replace('//', '/').split('/')
+                        const requestingUser = this.getRequestingUser(context)
+                        if (!requestingUser) return { errors: { user: ['not authorized'] } }
+                        const [, , , articleSlug, commentsOrFavorite, commentId] = (context?.url?.pathname ?? '').replace('//', '/').split('/')
                         if (!articleSlug) return
                         const author = {
                             username: requestingUser.username,
@@ -160,67 +173,69 @@ export default {
                         }
                         if (context.method === 'POST') {
                             if (commentsOrFavorite === 'comments') {
-                                if (!this.data.articles[articleSlug]) return
-                                this.data.comments[articleSlug] ||= []
-                                const comment = {
+                                if (!this.data('articles', articleSlug)) return { errors: { article: ['not found'] } }
+                                const articleComments = this.data('comments', articleSlug) ?? [], comment = {
                                     body: payload?.comment?.body,
-                                    id: this.data.comments[articleSlug].length + 1,
+                                    id: articleComments.length + 1,
                                     createdAt: new Date().toISOString(),
                                     updatedAt: new Date().toISOString(),
                                     author: { ...author }
                                 }
-                                this.data.comments[article.slug].push(comment)
+                                articleComments.push(comment)
+                                this.data('comments', articleSlug, articleComments)
                                 return { comment }
                             } else if (commentsOrFavorite === 'favorite') {
-                                const article = this.data.articles[articleSlug]
-                                if (!article) return
+                                const article = this.data('articles', articleSlug)
+                                if (!article) return { errors: { article: ['not found'] } }
                                 article.favorited ||= []
                                 article.favorited.push(requestingUser.username)
                                 article.favoritesCount += 1
                                 article.updatedAt = new Date().toISOString()
+                                this.data('articles', articleSlug, article)
                                 return { article }
                             } else {
                                 const article = {}
                                 for (const p of ['title', 'description', 'body']) article[p] = (payload?.article ?? {})[p]
                                 article.slug = article.title.trim().replaceAll(/[^a-z0-9]+/gi, '-').replaceAll(/\-+/, '-').toLowerCase()
-                                if (this.data.articles[article.slug]) return
+                                if (this.data('articles', article.slug)) return { errors: { article: ['already exists'] } }
                                 article.tagList = (payload?.article ?? {}).tagList ?? []
                                 article.createdAt = new Date().toISOString()
                                 article.updatedAt = new Date().toISOString()
                                 article.favorited = []
                                 article.favoritesCount = 0
                                 article.author = { ...author }
-                                this.data.articles[article.slug] = article
+                                this.data('articles', article.slug, article)
                                 return { article }
                             }
                         } else if (context.method === 'PUT') {
-                            const article = this.data.articles[articleSlug]
-                            if (!article) return
+                            const article = this.data('articles', articleSlug) ?? {}
+                            if (!Object.keys(article).length) return { errors: { article: ['not found'] } }
                             if (article.author?.username !== requestingUser.username) return
                             for (const p of ['title', 'description', 'body']) article[p] = (payload?.article ?? {})[p]
                             article.updatedAt = new Date().toISOString()
                             const oldSlug = articleSlug, newSlug = article.title.trim().replaceAll(/[^a-z0-9]+/gi, '-').replaceAll(/\-+/, '-').toLowerCase()
-                            this.data.articles[newSlug] = article
+                            this.data('articles', newSlug, article)
                             if (newSlug !== oldSlug) {
-                                this.data.comments[newSlug] = this.data.comments[oldSlug]
-                                delete this.data.articles[oldSlug]
+                                this.data('comments', newSlug, this.data('comments', oldSlug) ?? [])
+                                this.data('articles', oldSlug, null)
                             }
                             return { article }
                         } else if (context.method === 'DELETE') {
-                            if (!this.data.articles[articleSlug]) return
-                            if (article.author?.username !== requestingUser.username) return
+                            if (!this.data('articles', articleSlug)) return { errors: { article: ['not found'] } }
+                            if (article.author?.username !== requestingUser.username) return { errors: { article: ['not authorized'] } }
                             if (commentsOrFavorite === 'comments' && commentId) {
-                                let comments = this.data.comments[articleSlug] ?? []
+                                let comments = this.data('comments', articleSlug) ?? []
                                 comments = comments.filter(c => c.id != commentId)
-                                this.data.comments[articleSlug] = comments
+                                this.data('comments', articleSlug, comments)
                             } else if (commentsOrFavorite === 'favorite') {
-                                const article = this.data.articles[articleSlug]
+                                const article = this.data('articles', articleSlug)
                                 article.favorited ||= []
                                 article.favorited.push(requestingUser.username)
+                                this.data('articles', articleSlug, article)
                                 return { article }
                             } else {
-                                delete this.data.articles[articleSlug]
-                                delete this.data.comments[articleSlug]
+                                this.data('articles', articleSlug, null)
+                                this.data('comments', articleSlug, null)
                             }
                         }
                 }
@@ -230,7 +245,7 @@ export default {
                 switch (context.method) {
                     case 'GET':
                         let tags = {}
-                        let articleTags = Object.values(this.data.articles).forEach(a => { for (const t of (a.tagList ?? [])) tags[t] = true })
+                        let articleTags = Object.values(this.data('articles')).forEach(a => { for (const t of (a.tagList ?? [])) tags[t] = true })
                         tags = Object.keys(tags)
                         tags.sort()
                         return tags
