70d734e7a5d345629c635508523ce8a1528db602
David Pitman <david.pitman@cloudouble.com>
2023-11-28 23:49:14 +1000

    building

--- packages/main.js
+++ packages/main.js
@@ -1,10 +1,12 @@
-const filters = Object.fromEntries((new URLSearchParams(window.location.search)).entries()), { limit = 10, offset = 0 } = filters
+const filters = Object.fromEntries((new URLSearchParams(window.location.search)).entries()), { limit = 10, offset = 0 } = filters,
+    query = (new URLSearchParams({ ...filters, limit, offset })).toString()
 export default {
     gateways: {
         api: hostpath => `https://api.realworld.io/${hostpath}}`
     },
     variables: {
         limit, offset,
+        articleDefault: `api://api/articles?${query}`,
         articleListRender: ['{"`.article-list`": ($count($) ? ([$map($.articles, function($a) {{',
             '"`.article-meta meta[is=e-data]`@target": "api://api/articles/" & $a.slug & "/favorite",',
             '"`.article-meta a`@href": "./#profile/" & $a.author.username,',
@@ -21,8 +23,8 @@ export default {
             '"`.pagination`": [$map($.pages, function($page) { {"@class": ($page.active ? "page-item active" : "page-item"), "`meta`@transform": "{\\"@resource\\": \\"api://api/articles?" & $page.query & "\\"}", "`a`.textContent": $page.number } })]',
             '}'].join(''),
         tagListRender: '$map($, function($t) {{ ".textContent": $t, "@href": "./?tag=" & $t }})',
-        toggleFeedToYour: '{"`main|#article-feed-pointer`@resource": "api://api/articles/feed?' + (new URLSearchParams({ ...filters, limit, offset })).toString() + '", "`ul|li a.active`@class": "nav-link", "`li|a`@class": "nav-link active" }',
-        toggleFeedToGlobal: '{"`main|#article-feed-pointer`@resource": "api://api/articles?' + (new URLSearchParams({ ...filters, limit, offset })).toString() + '", "`ul|li a.active`@class": "nav-link", "`li|a`@class": "nav-link active" }',
+        toggleFeedToYour: '{"`main|#article-feed-pointer`@resource": "api://api/articles/feed?' + query + '", "`ul|li a.active`@class": "nav-link", "`li|a`@class": "nav-link active" }',
+        toggleFeedToGlobal: '{"`main|#article-feed-pointer`@resource": "api://api/articles?' + query + '", "`ul|li a.active`@class": "nav-link", "`li|a`@class": "nav-link active" }',
         authPostRender: ['{"`.error-messages`": $each($.errors, function($v, $k) {  $k & " " & $join($v, ", ") }),',
             '"`#user`.write()": $.user ? $.user : {}, "`#router`.to(./#profile)": $boolean($.user) }'].join('')
     }
