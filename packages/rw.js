fa25c65309a2da13c5b8ea5e2411a4d7a07bbcc7
David Pitman <david.pitman@cloudouble.com>
2023-12-12 18:54:32 +1000

    re-building

--- packages/rw.js
+++ packages/rw.js
@@ -1,5 +1,15 @@
 export default {
     variables: {
-        blankProfile: "https://upload.wikimedia.org/wikipedia/commons/e/e7/Ico_user_profile_blank.png"
+        blankProfile: "https://upload.wikimedia.org/wikipedia/commons/e/e7/Ico_user_profile_blank.png",
+        404: `
+                <div class="home-page">
+                    <div class="banner">
+                        <div class="container">
+                            <h1 class="logo-font">error 404</h1>
+                            <p>This is not the knowledge you are looking for &#x1F44B;</p>
+                        </div>
+                    </div>
+                </div>
+            `
     }
 }
